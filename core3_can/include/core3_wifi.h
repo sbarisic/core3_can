2355b7742bf564529326a30c3953ca58313953ca
Saša Barišić <sasa.barisic@serengetitech.com>
2024-08-26 13:49:11 +0200

    Wifi

---
+++ core3_can/include/core3_wifi.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+
+
+#if defined(__cplusplus)
+}
+#endif
---
+++ core3_can/src/core3_wifi.cpp
@@ -0,0 +1,115 @@
+#include <core3_wifi.h>
+
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include <string.h>
+// #include "nvs_flash.h"
+#include <esp_now.h>
+
+#include "lwip/err.h"
+#include "lwip/sys.h"
+
+#define dprintf printf
+
+#define EXAMPLE_ESP_WIFI_SSID "CONFIG_ESP_WIFI_SSID"
+#define EXAMPLE_ESP_WIFI_PASS "CONFIG_ESP_WIFI_PASSWORD"
+#define EXAMPLE_ESP_MAXIMUM_RETRY 3
+
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
+
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START)
+    {
+        esp_wifi_connect();
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED)
+    {
+        if (s_retry_num < EXAMPLE_ESP_MAXIMUM_RETRY)
+        {
+            esp_wifi_connect();
+            s_retry_num++;
+            dprintf("retry to connect to the AP");
+        }
+        else
+        {
+            xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+        }
+        dprintf("connect to the AP fail");
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP)
+    {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        dprintf("got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+    }
+}
+
+void core3_wifi_init()
+{
+    s_wifi_event_group = xEventGroupCreate();
+
+    // Init TCP/IP stack
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+
+    wifi_config_t wifi_config = {0};
+    wifi_sta_config_t sta = {0};
+
+    sta.ssid = EXAMPLE_ESP_WIFI_SSID;
+    sta.password = EXAMPLE_ESP_WIFI_PASS;
+    sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
+    sta.sae_pwe_h2e = WPA3_SAE_PWE_UNSPECIFIED;
+    sta.sae_h2e_identifier = "H2E Ident";
+    wifi_config.sta = sta;
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    dprintf("wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits =
+        xEventGroupWaitBits(s_wifi_event_group, WIFI_CONNECTED_BIT | WIFI_FAIL_BIT, pdFALSE, pdFALSE, portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT)
+    {
+        dprintf("connected to ap SSID:%s password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    }
+    else if (bits & WIFI_FAIL_BIT)
+    {
+        dprintf("Failed to connect to SSID:%s, password:%s", EXAMPLE_ESP_WIFI_SSID, EXAMPLE_ESP_WIFI_PASS);
+    }
+    else
+    {
+        dprintf("UNEXPECTED EVENT");
+    }
+}
