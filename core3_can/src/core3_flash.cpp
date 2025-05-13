71c95094102b6f7843ce1e9c533f5edb851413ac
Saša Barišić <cartmanium@gmail.com>
2025-05-14 00:11:52 +0200

    Small refactoring

--- core3_can/src/core3_flash.cpp
+++ core3_can/src/core3_flash.cpp
@@ -9,33 +9,39 @@
 #include "spi_flash_mmap.h"
 #include "esp_partition.h"
 
-static size_t cal_addr_physical = 0x300000;
 static size_t mem_size = 0x10000;
+static esp_partition_mmap_handle_t part_mmap_handle;
+static const esp_partition_t *cal_part;
+static const void *cal_memory;
 
-const esp_partition_t *cal_part;
-const void *cal_memory;
-
-void core3_flash_cal_erase()
+bool core3_flash_cal_erase()
 {
-    esp_partition_erase_range(cal_part, 0x0, mem_size);
+    esp_err_t err = esp_partition_erase_range(cal_part, 0x0, mem_size);
+
+    if (err != ESP_OK)
+    {
+        dprintf("esp_partition_erase_range() failed\n");
+        return false;
+    }
+
+    return true;
 }
 
-void core3_flash_cal_write(size_t offset, const void *src, size_t size)
+bool core3_flash_cal_write(size_t offset, const void *src, size_t size)
 {
     esp_err_t err = esp_partition_write(cal_part, 0x0, src, size);
 
     if (err != ESP_OK)
     {
         dprintf("core3_flash_cal_write() failed\n");
+        return false;
     }
+
+    return true;
 }
 
-bool core3_flash_init()
+bool core3_flash_map()
 {
-
-    cal_part = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_UNDEFINED, "cal");
-
-    esp_partition_mmap_handle_t part_mmap_handle;
     esp_err_t err = esp_partition_mmap(cal_part, 0x0, mem_size, ESP_PARTITION_MMAP_DATA, &cal_memory, &part_mmap_handle);
 
     if (err != ESP_OK)
@@ -43,6 +49,20 @@ bool core3_flash_init()
         dprintf("esp_partition_mmap() failed\n");
         return false;
     }
+
+    return true;
+}
+
+void core3_flash_unmap()
+{
+    esp_partition_munmap(part_mmap_handle);
+}
+
+bool core3_flash_init()
+{
+    cal_part = esp_partition_find_first(ESP_PARTITION_TYPE_DATA, ESP_PARTITION_SUBTYPE_DATA_UNDEFINED, "cal");
+    if (!core3_flash_map())
+        return false;
 
     dprintf("core3_flash_init() done\n");
     return true;
